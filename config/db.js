c47448b803a1dc2e79de3dd2c5dbc37260ea6120
Aryamaan <pandeyaryamaan@gmail.com>
2021-03-02 14:56:38 +0530

    Committed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const config=require("config");
 const db=config.get("mongoURI");
 const connectDB= async () =>{
     try{
-        await mongoose.connect(db,{useNewUrlParser:true})
+        await mongoose.connect(db,{useNewUrlParser:true,
+        useCreateIndex:true}, {useUnifiedTopology: true});
         console.log("mongoDB Connected.....");
 
     }catch(err){
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app=express()
 //Connect Database
 connectDB();
 
+//Init MiddleWare
+app.use(express.json({extended:false}));
 app.get('/',(req,res)=> res.send("API Running"));
 
 //Define Routes
